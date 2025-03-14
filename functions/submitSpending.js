bc834241bc8f6a2a5aefe50223f94a872452b17e
tuanqchau <tuanqchau2810@gmail.com>
2025-03-13 21:00:10 -0400

    ughhh

--- functions/submitSpending.js
+++ functions/submitSpending.js
@@ -6,7 +6,7 @@ dotenv.config();
 // Google Sheets API authentication
 const auth = new google.auth.GoogleAuth({
     credentials: {
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
         client_email: process.env.GOOGLE_CLIENT_EMAIL
     },  
     scopes: 'https://www.googleapis.com/auth/spreadsheets',
